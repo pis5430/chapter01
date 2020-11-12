c5bf5ec89dd9bf738fd6b82b93d41961c2a4f909
pyo i seul <isl1089@naver.com>
2020-11-12 17:06:38 +0900

    11/12 수업 02

--- src/com/javaex/ex03/Ex07.java
+++ src/com/javaex/ex03/Ex07.java
@@ -7,11 +7,11 @@ public class Ex07 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
+		/*
 		System.out.println("숫자를 입력하세요");
 		System.out.print("숫자: ");
 		
 		int num = sc.nextInt();
-		
 		int even_num = num%2; //나머지가 0이면 짝수 1이면 홀수
 	
 
@@ -24,6 +24,34 @@ public class Ex07 {
 		}else{
 			System.out.println("0");			
 		}
+		*/
+		
+		
+		
+		// if안에 if가 들어가는 방법
+		System.out.println("숫자를 입력하세요");
+		System.out.print("숫자: ");
+		
+		int num = sc.nextInt();
+		
+		int even_num = num%2; //나머지가 0이면 짝수 1이면 홀수
+		
+
+		if(num>0) {
+			if(even_num==0) {
+				System.out.println("짝수");
+			}else {
+				System.out.println("홀수");				
+			}
+
+		}else if(num<0){
+			System.out.println("음수");	
+		}else {
+			System.out.println("0");	
+		}
+		
+		
+		
 		
 		
 		sc.close();
