d14a54f52ed75244b36751a1d4506cfd52e3ba0e
pyo i seul <isl1089@naver.com>
2020-11-20 11:18:06 +0900

    11/20 배열수업 1st commit

--- src/com/javaex/ex05/Ex08.java
+++ src/com/javaex/ex05/Ex08.java
@@ -11,14 +11,11 @@ public class Ex08 {
 		for(int i = 0; i<leftArray.length; i++) {
 			
 			if(leftArray==rightArray) {
-
 			}else if(leftArray.length!=rightArray.length){
 				System.out.println("두 배열의 크기가 다릅니다");
 				break;
 			}else if (leftArray.length==rightArray.length) {
-				
 				if(leftArray[i]!=rightArray[i]) {
-					
 					System.out.println(i + "번째 값이 다릅니다.");
 				}
 			}
